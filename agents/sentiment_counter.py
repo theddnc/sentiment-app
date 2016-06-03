1b4dfff1bc22a607a22cb75401f38624e9fb60ff
Agata Dębska <adebska@iterato.rs>
2016-06-03 16:04:59 +0200

    Bing search

--- agents/sentiment_counter.py
+++ agents/sentiment_counter.py
@@ -1,13 +1,14 @@
 import nltk
 from googleapiclient.discovery import build
 import math
+import urllib2
+import json
+import pprint
 
 class SentimentCounter(object):
     def __init__(self):
         self.sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
         self.tokenizer = nltk.tokenize.TweetTokenizer()
-        self.service = build("customsearch", "v1",
-                        developerKey="AIzaSyC7dxUlRswzAw87teCbOq4TE-UANiSErFE")
 
     # function which tokenize text (ex. tweet) into list of lists of words
     def split_paragraph_into_words(self, text):
@@ -22,51 +23,50 @@ class SentimentCounter(object):
     def split_into_two_words_phrases(self, sentence):
         phrases = []
         for index in range(len(sentence)):
-            if sentence[index][1] == 'JJ':
-                if sentence[index+1] and (sentence[index+1][1] == 'NN' or sentence[index+1][1] == 'NNS'):
+            if sentence[index][1] == 'JJ' or sentence[index][1] == 'JJR' or sentence[index][1] == 'JJS' or sentence[index][1] == 'RB' or sentence[index][1] == 'RBR' or sentence[index][1] == 'RBS':
+                if index+1 < len(sentence) and (sentence[index+1][1] == 'NN' or sentence[index+1][1] == 'NNS' or sentence[index+1][1] == 'NNP' or sentence[index+1][1] == 'NNPS'):
                     phrases.append([sentence[index], sentence[index+1]])
-                elif sentence[index+1] and sentence[index+1][1] == 'JJ':
-                    if (sentence[index+2] and (sentence[index+2][1] != 'NN' and sentence[index+2][1] != 'NNS')) or not sentence[index+2]:
-                        phrases.append([sentence[index], sentence[index+1]])
-            if sentence[index][1] == 'RB' or sentence[index][1] == 'RBR' or sentence[index][1] == 'RBS':
-                if sentence[index + 1] and sentence[index + 1][1] == 'JJ':
-                    if (sentence[index + 2] and (sentence[index + 2][1] != 'NN' and sentence[index + 2][1] != 'NNS')) or not sentence[index + 2]:
-                        phrases.append([sentence[index], sentence[index + 1]])
-                elif sentence[index + 1] and (sentence[index + 1][1] == 'VB' or sentence[index + 1][1] == 'VBD' or sentence[index + 1][1] == 'VBN' or sentence[index + 1][1] == 'VBG'):
-                    phrases.append([sentence[index], sentence[index + 1]])
-            if sentence[index][1] == 'NN' or sentence[index][1] == 'NNS':
-                if sentence[index + 1] and sentence[index + 1][1] == 'JJ':
-                    if (sentence[index + 2] and (sentence[index + 2][1] != 'NN' and sentence[index + 2][1] != 'NNS')) or not sentence[index + 2]:
-                                phrases.append([sentence[index], sentence[index + 1]])
         return phrases
 
     def count_sentiment_for_phrase(self, phrase):
-        phrase_near_poor = self.service.cse().list(
-            q='"poor" '+phrase,
-            cx='000797812240645302824:8o6spe9gkxa',
-        ).execute()['searchInformation']['totalResults']
-        phrase_near_poor = float(phrase_near_poor)
+        keyBing = 'AXxEt0tlz+z8DRMf8PIW7vH5llUGdSHqv20KF5YwplA'  # get Bing key from: https://datamarket.azure.com/account/keys
+        credentialBing = 'Basic ' + (':%s' % keyBing).encode('base64')[:-1]  # the "-1" is to remove the trailing "\n" which encode adds
+        query = '%22'+phrase+'%22%22poor%22'
+        url = 'https://api.datamarket.azure.com/Bing/Search/Composite?Sources=%27web%27&Query=%27' + query + '%27&$top=1&$format=JSON'
+        request = urllib2.Request(url)
+        request.add_header('Authorization', credentialBing)
+        requestOpener = urllib2.build_opener()
+        response = requestOpener.open(request)
+        results = json.load(response)
+        phrase_near_poor =  float(results['d']['results'][0]['WebTotal'])
 
-        phrase_near_excellent = self.service.cse().list(
-            q='"excellent" '+phrase,
-            exactTerms=phrase,
-            cx='000797812240645302824:8o6spe9gkxa',
-        ).execute()['searchInformation']['totalResults']
-        phrase_near_excellent = float(phrase_near_excellent)
+        query = '%22' + phrase + '%22%22excellent%22'
+        url = 'https://api.datamarket.azure.com/Bing/Search/Composite?Sources=%27web%27&Query=%27' + query + '%27&$top=1&$format=JSON'
+        request = urllib2.Request(url)
+        request.add_header('Authorization', credentialBing)
+        requestOpener = urllib2.build_opener()
+        response = requestOpener.open(request)
+        results = json.load(response)
+        phrase_near_excellent = float(results['d']['results'][0]['WebTotal'])
 
-        excellent = self.service.cse().list(
-            q='"excellent"',
-            exactTerms=phrase,
-            cx='000797812240645302824:8o6spe9gkxa',
-        ).execute()['searchInformation']['totalResults']
-        excellent = float(excellent)
+        query = '%22excellent%22'
+        url = 'https://api.datamarket.azure.com/Bing/Search/Composite?Sources=%27web%27&Query=%27' + query + '%27&$top=1&$format=JSON'
+        request = urllib2.Request(url)
+        request.add_header('Authorization', credentialBing)
+        requestOpener = urllib2.build_opener()
+        response = requestOpener.open(request)
+        results = json.load(response)
+        excellent = float(results['d']['results'][0]['WebTotal'])
 
-        poor = self.service.cse().list(
-            q='"poor"',
-            exactTerms=phrase,
-            cx='000797812240645302824:8o6spe9gkxa',
-        ).execute()['searchInformation']['totalResults']
-        poor = float(poor)
+        query = '%22poor%22'
+        url = 'https://api.datamarket.azure.com/Bing/Search/Composite?Sources=%27web%27&Query=%27' + query + '%27&$top=1&$format=JSON'
+        request = urllib2.Request(url)
+        request.add_header('Authorization', credentialBing)
+        requestOpener = urllib2.build_opener()
+        response = requestOpener.open(request)
+        results = json.load(response)
+        poor = float(results['d']['results'][0]['WebTotal'])
+
         return math.log((phrase_near_excellent * poor)/(phrase_near_poor * excellent), 2)
 
     def count_sentiment(self, text):
@@ -74,15 +74,14 @@ class SentimentCounter(object):
         tagged_sentences = self.pos_tag(tokenized_sentences)
         phrases = [self.split_into_two_words_phrases(sentence) for sentence in tagged_sentences][0]
         sentiment = 0.0
+        print phrases
         for phrase in phrases:
-            query = phrase[0][0]+' ' + phrase[1][0]
+            query = phrase[0][0]+'+' + phrase[1][0]
+            print query
             sentiment += self.count_sentiment_for_phrase(query)
         return sentiment
 
-# text = """The third step is to calculate the average semantic
-# orientation of the phrases in the given review
-# and classify the review as recommended if the average
-# is positive and otherwise not recommended.
-# """
+# text = """good job
+# # """
 # s = SentimentCounter()
 # print s.count_sentiment(text)
